e4ec84de4a34de74e51dffb00616facef4d1203c
Nasimul650 <70124945+Nasimul650@users.noreply.github.com>
2022-04-07 09:33:11 +0600

    Initial commit

---
+++ components/PostWidget.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import Link from 'next/link';
+import { getRecentPosts, getSimilarPosts } from '../services';
+
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+      if(slug){
+        getSimilarPosts(categories, slug)
+          .then((result) =>{ setRelatedPosts(result)});
+      } else{
+        getRecentPosts().then((result) =>{ setRelatedPosts(result)})
+      }
+  }, [slug])
+  // console.log(relatedPosts)
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+        <h3 className='text-xl mb-8 font-semibold border-b pb-3'>
+          {slug ? 'Related Posts' : "Recent Posts"}
+        </h3>
+        {relatedPosts.map((post) => (
+          <div key={post.title} className="flex items-center w-full mb-4">
+              <div className='w-16 flex-none'>
+                  <img src={post.featuredimage.url} className='align-middle rounded-full' height='100px' width='100px' alt={post.title} />
+              </div>
+              <div className='flex-grow ml-4'>
+                <p className='text-gray-500 font-xs'>
+                  {moment(post.createdAt).format('MMM DD, YYYY')}
+                </p>
+                <h1 className='font-semibold transition hover:text-pink-600'>
+                <Link  href={`/post/${post.slug}`} key={post.title} >
+                  {post.title}
+                </Link>
+                </h1>
+              </div>
+          </div>
+        ))}
+    </div>
+  )
+}
+
+export default PostWidget
