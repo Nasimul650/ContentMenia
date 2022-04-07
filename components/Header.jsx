1eb3d1078ae761b2b4d9ce1456a560586c2b900e
Nasimul650 <70124945+Nasimul650@users.noreply.github.com>
2022-04-07 09:42:57 +0600

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,8 +9,8 @@ const Header = () => {
     <div className='container mx-auto px-10 mb-8'>
         <div className='border-b w-full inline-block border-blue-400 py-8'>
             <div className='md:float-left block'>
-                <Link href="">
-                    <span className='cursor-pointer font-bold text-4xl text-white'>
+                <Link href="/">
+                    <span className='cursor-pointer font-bold text-4xl text-white transition duration-700 text-center hover:text-pink-400'>
                          GraphCMS
                     </span>
                 </Link>
@@ -18,7 +18,7 @@ const Header = () => {
             <div className='hidden md:float-left md:contents'>
                     {categories.map((category)=>(
                         <Link key={category.slug} href={`/category/${category.slug}`}>
-                            <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
+                            <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer transition duration-300 hover:text-pink-400'>
                                 {category.name}
                             </span>
                         </Link>
